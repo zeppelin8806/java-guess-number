dc1b811abd36c60bd68ac66d6fe4def10e156afa
Bobby Hintz <robert.hintz.18@gmail.com>
2023-12-27 16:13:10 -0500

    Week 2 Command Line 3/4 Progress

--- module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,25 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String value = input.nextLine();
+		int decimal = Integer.parseInt(value);
+
+		int remain = 0;
+
+		for(int i = 0; i<10; i++){
+			int valueNew;
+			valueNew = decimal / 2;
+			if(decimal%2 == remain)
+		}
+
+
 
 	}
 
--- module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,37 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
+		System.out.print("Please enter a number: ");
+		String value = input.nextLine();
+		int number = Integer.parseInt(value);
+
+		int n1 = 0;
+		int n2 = 1;
+		int n3 = 2;
+		String fib = "0,1";
+
+		if(number <= 0 ){
+			System.out.println("0,1");
+		} else if(number == 1){
+			System.out.println("0,1,1");
+		} else{
+			for (int i = 2; n3 < number; i++) {
+				n3 = n1 + n2;
+				if (number < n3){
+					break;
+				};
+				fib = fib + "," + Integer.toString(n3);
+				n1 = n2;
+				n2 = n3;
+			}
+		}
+		System.out.println(fib);
 	}
 
 }
