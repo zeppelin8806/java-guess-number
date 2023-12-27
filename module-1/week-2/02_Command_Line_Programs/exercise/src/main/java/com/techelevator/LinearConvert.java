2cdb7f922286a1b1da3d6cab769634a8cbe4d2a1
Bobby Hintz <robert.hintz.18@gmail.com>
2023-12-27 14:58:59 -0500

    Week 2 Command line progress

--- module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,28 @@
 package com.techelevator;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Please enter the length: ");
+		String value = input.nextLine();
+		int length = Integer.parseInt(value);
+
+		System.out.print("Is the measurement in (m)eter, or (f)eet? ");
+		value = input.nextLine();
+		String unit = value;
+
+		if(unit.equals("m")){
+			int metersToFeet = (int)(length * 3.2808399);
+			System.out.println(length + "m is approximately" + metersToFeet + "f");
+		} else if (unit.equals("f")){
+			int feetToMeters = (int)(length * 0.3048 );
+			System.out.println(length + "f is approximately " + feetToMeters + "m");
+		}
 
 	}
-
 }
--- module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,26 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
+		System.out.print("Please enter the temperature: ");
+		String value = input.nextLine();
+		int temperature = Integer.parseInt(value);
+
+		System.out.print("Is the temperature in (C)elsius, or (F)ahrenheit?");
+		value = input.nextLine();
+		String unit = value;
+
+		if (unit.equals("C")) {
+			double temperatureFarenheit = (temperature * 1.8) + 32;
+			System.out.println(temperature + unit + "is " + temperatureFarenheit + "F");
+		} else if (unit.equals("F")) {
+			int temperatureCelsius = (int)((temperature - 32) / 1.8);
+			System.out.println(temperature + unit + "is " + temperatureCelsius + "C");
+		}
 	}
-
 }
