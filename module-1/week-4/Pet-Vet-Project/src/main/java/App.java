798a63d7efda9a686dbb30a8a3ae302cab8cc88b
Bobby Hintz <robert.hintz.18@gmail.com>
2024-01-19 15:31:48 -0500

    PetVet project status 1.0

---
+++ module-1/week-4/Pet-Vet-Project/src/main/java/App.java
@@ -0,0 +1,162 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.*;
+
+public class App {
+
+    private static final String FIELD_DELIMITER = "\\|";
+    private static final int SPECIES_FIELD = 0;
+    private static final int OWNER_FIELD = 1;
+    private static final int PET_NAME_FIELD = 2;
+    private static final int WEIGHT_FIELD = 3;
+    private final Scanner keyboard = new Scanner(System.in);
+    private List<String> species = new ArrayList<>();
+    private List<String> owner = new ArrayList<>();
+    private List<Integer> petName = new ArrayList<>();
+    private List<Double> weight = new ArrayList<>();
+
+    public static void main(String[] args){
+
+        App app = new App();
+        app.loadData();
+        app.run();
+    }
+
+    private void loadData() {
+
+        String[] dataset = Dataset.load();
+        for (int i = 0; i < dataset.length; i++) {
+            String[] tempset = dataset[i].split(FIELD_DELIMITER);
+            species.add(tempset[SPECIES_FIELD]);
+            owner.add(tempset[OWNER_FIELD]);
+            petName.add(Integer.parseInt(tempset[PET_NAME_FIELD]));
+            weight.add(Double.parseDouble(tempset[WEIGHT_FIELD]));
+        }
+    }
+
+    private void run(){
+
+    }
+
+    private void printMainMenu() {
+        System.out.println("1: Display data and subsets");
+        System.out.println("2: Search books");
+        System.out.println("0: Exit");
+        System.out.println();
+    }
+
+    private void printSearchBooksMenu() {
+        System.out.println("1: Search by title");
+        System.out.println("2: Search by author");
+        System.out.println("3: Search by published year");
+        System.out.println("4: Search by published year range");
+        System.out.println("5: Find most recent books");
+        System.out.println("6: Search by price");
+        System.out.println("7: Search by price range");
+        System.out.println("8: Find least expensive books");
+        System.out.println("0: Return to main menu");
+        System.out.println();
+    }
+
+    private void displayDataset(String[] dataset) {
+        System.out.println("Dataset");
+        System.out.println("-------");
+        for (String data : dataset) {
+            System.out.println(data);
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayTitlesList(List<String> titles) {
+        System.out.println("Titles");
+        System.out.println("-------");
+        for (int i = 0; i < titles.size(); i++) {
+            System.out.println(i + ": " + titles.get(i));
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayAuthorsList(List<String> authors) {
+        System.out.println("Authors");
+        System.out.println("-------");
+        for (int i = 0; i < authors.size(); i++) {
+            System.out.println(i + ": " + authors.get(i));
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayPublishedYearsList(List<Integer> publishedYears) {
+        System.out.println("Published Years");
+        System.out.println("---------------");
+        for (int i = 0; i < publishedYears.size(); i++) {
+            System.out.println(i + ": " + publishedYears.get(i));
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayPricesList(List<BigDecimal> prices) {
+        System.out.println("Prices");
+        System.out.println("------");
+        for (int i = 0; i < prices.size(); i++) {
+            System.out.println(i + ": " + prices.get(i));
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private int promptForMenuSelection(String prompt) {
+        System.out.print(prompt);
+        int menuSelection;
+        try {
+            menuSelection = Integer.parseInt(keyboard.nextLine());
+        } catch (NumberFormatException e) {
+            menuSelection = -1;
+        }
+        return menuSelection;
+    }
+
+    private String promptForString(String prompt) {
+        System.out.print(prompt);
+        return keyboard.nextLine();
+    }
+
+    private int promptForPublishedYear(String prompt) {
+        int year;
+        while (true) {
+            System.out.println(prompt);
+            try {
+                year = Integer.parseInt(keyboard.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("The year provided is not well-formed. It must be YYYY.");
+            }
+        }
+        return year;
+    }
+
+    private double promptForPrice(String prompt) {
+        double price;
+        while (true) {
+            System.out.println(prompt);
+            try {
+                price = Double.parseDouble(keyboard.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("The price provided is not a valid monetary value.");
+            }
+        }
+        return price;
+    }
+
+    private void promptForReturn() {
+        System.out.println("Press RETURN to continue.");
+        keyboard.nextLine();
+    }
+
+}
---
+++ module-1/week-4/Pet-Vet-Project/src/main/java/Dataset.java
@@ -0,0 +1,35 @@
+public class Dataset {
+
+    public static String[] load(){
+        return new String[] {
+                "Dog|Chelsea|12|12.7",
+                "Cat|Chelsea|4|8.5",
+                "Dog|Selena|3|35.0",
+                "Dog|John|5|25.5",
+                "Cat|Alice|16|10.2",
+                "Fish|Bob|2|0.5",
+                "Bird|Emma|3|1.8",
+                "Dog|Mike|8|30.0",
+                "Cat|Sophia|1|8.7",
+                "Fish|David|3|0.3",
+                "Bird|Olivia|6|2.3",
+                "Dog|Daniel|6|22.1",
+                "Cat|Emily|17|11.5",
+                "Fish|Andrew|1|0.7",
+                "Bird|Abigail|4|1.5",
+                "Dog|James|10|27.8",
+                "Cat|Ella|5|9.4",
+                "Fish|William|5|0.6",
+                "Bird|Ava|3|2.0",
+                "Dog|Sophie|2|29.3",
+                "Cat|Benjamin|9|10.8",
+                "Fish|Charlotte|1|0.9",
+                "Bird|Liam|6|1.2",
+                "Dog|Grace|13|26.4",
+                "Cat|Logan|14|12.1",
+                "Fish|Harper|4|0.4",
+                "Bird|Aiden|1|1.9",
+                "Dog|Nora|7|24.7"
+        };
+    }
+}
