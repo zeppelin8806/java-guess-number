f159b59a5639623bff7b3e0bfa32ef90f9098fbe
Bobby Hintz <robert.hintz.18@gmail.com>
2024-01-28 18:01:54 -0500

    Week 5 progress 1.2

--- module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -2,5 +2,46 @@ package com.techelevator;
 
 public class Airplane {
 
+    private String planeNumber;
+    private int totalFirstClassSeats;
+    private int bookedFirstClassSeats;
+    private int totalCoachSeats;
+    private int bookedCoachSeats;
 
+    public String getPlaneNumber() {
+        return planeNumber;
+    }
+    public int getTotalFirstClassSeats(){
+        return totalFirstClassSeats;
+    }
+    public int getBookedFirstClassSeats() {
+        return bookedFirstClassSeats;
+    }
+    public int getAvailableFirstClassSeats(){
+        return bookedFirstClassSeats-totalFirstClassSeats;
+    }
+
+    public int getTotalCoachSeats() {
+        return totalCoachSeats;
+    }
+
+    public int getBookedCoachSeats() {
+        return bookedCoachSeats;
+    }
+    public int getAvailableCoachSeats(){
+        return bookedCoachSeats-totalCoachSeats;
+    }
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats){
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats = totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
+    public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats){
+        if(forFirstClass){
+            bookedFirstClassSeats += totalNumberOfSeats;
+        } else{
+            bookedCoachSeats += totalNumberOfSeats;
+        }
+        return false;
+    }
 }
--- module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,68 @@
 package com.techelevator;
 
 public class Television {
+    private boolean isOn;
+    private int currentChannel;
+    private int currentVolume;
 
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public int getCurrentVolume() {
+        if(isOn){
+            return currentVolume;
+        } else return 0;
+    }
+    public void turnOff(){
+        isOn = false;
+    }
+    public void turnOn(){
+        isOn = true;
+        currentChannel = 3;
+        currentVolume = 2;
+    }
+    public void changeChannel(int newChannel){
+        if(isOn){
+            if(newChannel>=3 && newChannel<= 18){
+                currentChannel = newChannel;
+            }
+        }
+    }
+    public void channelUp(){
+        if(isOn){
+            currentChannel++;
+            if(currentChannel>18){
+                currentChannel=3;
+            }
+        }
+    }
+    public void channelDown(){
+        if(isOn){
+            currentChannel--;
+            if(currentChannel<3){
+                currentChannel=18;
+            }
+        }
+    }
+    public void raiseVolume(){
+        if(isOn){
+            currentVolume++;
+            if(currentVolume>10){
+                currentVolume = 10;
+            }
+        }
+    }
+    public void lowerVolume(){
+        if(isOn){
+            currentVolume--;
+            if(currentVolume<0){
+                currentVolume=0;
+            }
+        }
+    }
 }
