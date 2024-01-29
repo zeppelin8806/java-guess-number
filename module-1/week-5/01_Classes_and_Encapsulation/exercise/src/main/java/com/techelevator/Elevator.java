74baae5053f0069b4534ed39289a9ecb943502bf
Bobby Hintz <robert.hintz.18@gmail.com>
2024-01-28 20:57:06 -0500

    Week 5 progress 1.3

--- module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
+++ module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
@@ -1,5 +1,49 @@
 package com.techelevator;
 
 public class Elevator {
+    private int currentFloor;
+    private int numberOfFloors;
+    private boolean doorOpen;
 
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public boolean isDoorOpen() {
+        return doorOpen;
+    }
+    public Elevator(int numberOfLevels){
+        getCurrentFloor();
+        currentFloor = 1;
+        getNumberOfFloors();
+        numberOfFloors = numberOfLevels;
+    }
+    public void openDoor(){
+        doorOpen = true;
+    }
+    public void closeDoor(){
+        doorOpen = false;
+    }
+    public void goUp(int desiredFloor){
+        openDoor();
+        if(desiredFloor>currentFloor && desiredFloor<=numberOfFloors){
+            closeDoor();
+            currentFloor=desiredFloor;
+        } else if(desiredFloor == currentFloor){
+            openDoor();
+        }
+    }
+    public void goDown(int desiredFloor){
+        openDoor();
+        if(desiredFloor<currentFloor && desiredFloor>0){
+            closeDoor();
+            currentFloor=desiredFloor;
+        } else if(desiredFloor == currentFloor){
+            openDoor();
+        }
+    }
 }
